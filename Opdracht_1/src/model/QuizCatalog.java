3d4fa0c803961362a803f6bb29d84014e5aff3a5
java <java@ubuntu.(none)>
2013-11-03 21:14:12 +0100

    update

--- Opdracht_1/src/model/QuizCatalog.java
+++ Opdracht_1/src/model/QuizCatalog.java
@@ -8,16 +8,35 @@ import java.util.ArrayList;
  * @version 25/10
  * 
  */
-public class QuizCatalog {
-        private ArrayList<Quiz> quizCatalogs = new ArrayList<Quiz>();        
 
-        public ArrayList<Quiz> getQuizCatalogs(){
-                return quizCatalogs;
-        }
+public class QuizCatalog implements Comparable<QuizCatalog>, Cloneable{
+	
+        private ArrayList<Quiz> quizCatalogs = new ArrayList<Quiz>();        
+        
+        
+        public QuizCatalog() throws IllegalArgumentException{
+    		this.setQuizCatalogs(new ArrayList<Quiz>());
+    	}
+    	
+    	public QuizCatalog(ArrayList<Quiz> quizCatalogs) throws IllegalArgumentException{
+    		this.setQuizCatalogs(quizCatalogs);
+    	}
+    	
+    	//get & set
+    	
+    	public ArrayList<Quiz> getQuizCatalogs(){
+            return quizCatalogs;
+    }
+        
+        public void setQuizCatalogs(ArrayList<Quiz> quizCatalogs) throws IllegalArgumentException{
+        	if (quizCatalogs == null)throw new IllegalArgumentException("Quiz Catalog is null");
+			this.quizCatalogs = quizCatalogs;
+		}
+
         
         //toevoegen
-        
-        public void addQuiz(Quiz quiz) throws Exception{
+
+		public void addQuiz(Quiz quiz) throws Exception{
                 try{
                         
                         for(int i = 0; i < this.quizCatalogs.size(); i++){
@@ -44,4 +63,37 @@ public class QuizCatalog {
                 }
                 return catalog;
         }
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result
+					+ ((quizCatalogs == null) ? 0 : quizCatalogs.hashCode());
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			QuizCatalog other = (QuizCatalog) obj;
+			if (quizCatalogs == null) {
+				if (other.quizCatalogs != null)
+					return false;
+			} else if (!quizCatalogs.equals(other.quizCatalogs))
+				return false;
+			return true;
+		}
+
+		@Override
+		public int compareTo(QuizCatalog o) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+        
 }
