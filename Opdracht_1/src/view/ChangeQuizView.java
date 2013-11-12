f3aee643f18484e07e82b42e61d5f86548f480bc
java <java@ubuntu.(none)>
2013-11-12 20:35:48 +0100

    update view

--- Opdracht_1/src/view/ChangeQuizView.java
+++ Opdracht_1/src/view/ChangeQuizView.java
@@ -6,12 +6,20 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
+import model.Quiz;
 import model.QuizCatalog;
 import model.ExerciseCatalog;
 
@@ -31,7 +39,14 @@ public class ChangeQuizView extends JFrame implements ActionListener{
 		private JTextField txt_01, txt_02;
 		private JLabel lb_01, lb_02, lb_03, lb_04;
 		private JTable table;
-		private JButton btn_update;
+		private JButton btn_update, btn_edit, btn_delete;
+		private JList listExercise;
+        private JList listQuiz;
+        private JComboBox<String> comboCategory;
+        private JComboBox<String> comboStatus;
+        private JComboBox<Integer> comboLeerjaar;
+        //private final JScrollPane paneExercise;
+        //private final JScrollPane paneQuiz;
 		
 		public ChangeQuizView()
 		{
@@ -50,42 +65,91 @@ public class ChangeQuizView extends JFrame implements ActionListener{
 			panel_02 = new JPanel();
 			txt_01 = new JTextField("Find quiz", 25);
 			txt_02 = new JTextField("Find exercise", 15);
+			
 			lb_01 = new JLabel("Status");
+			lb_01.setBounds(36, 24, 112, 15);
+			getContentPane().add(lb_01);
+			
+			comboStatus = new JComboBox<String>();
+			comboStatus.addItem("UNDER CONSTRUCTION");
+			comboStatus.addItem("COMPLETED");
+			comboStatus.addItem("READY");
+			comboStatus.addItem("LAST CHANCE");
+			comboStatus.addItem("CLOSED");
+			
+			comboLeerjaar = new JComboBox<Integer>();
+			
+			comboCategory = new JComboBox<String>();
+			
+			listExercise = new JList<>();
+			listExercise.setVisibleRowCount(10);
+			listExercise.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+			
+			listQuiz = new JList<>();
+			listQuiz.setVisibleRowCount(10);
+			listQuiz.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+               
 			lb_02 = new JLabel("Show category");
 			lb_03 = new JLabel("Quiz");
 			lb_04 = new JLabel("Exercise");
 			table = new JTable();
-			btn_update = new JButton("Search");
 			
+			btn_update = new JButton("Search");
+			btn_update.addActionListener(this);
+			
+			btn_edit = new JButton("Edit");
+			btn_edit.addActionListener(this);
+			
+			btn_delete = new JButton("Delete");
+			btn_delete.addActionListener(this);
+			
+			this.quizCatalog.readQuizzesFromFile();
+			this.exCatalog.readExercisesFromFile();
+			
+			/*
 			//implements interface Actionlistener
 			btn_update.addActionListener(new ActionListener() {
 
 				@Override
 				public void actionPerformed(ActionEvent e) {
-					System.out.println("Update!");
-					//uit te voeren code
-					
+					System.out.println("Search!");
+					//uit te voeren code					
 				}
-				
 			});
-			
+			*/
 			
 			//layout
 			
 			panel_01.add(txt_01);
 			panel_01.add(txt_02);
 			panel_01.add(lb_01);
-			
 			panel_01.add(btn_update);
-			
+			panel_01.add(btn_edit);
+			panel_01.add(btn_delete);
 			panel_01.add(lb_02);
-			
 			panel_02.add(lb_03);
 			panel_02.add(lb_04);
 			panel_02.add(table);
+			panel_02.add(comboStatus);
+			panel_02.add(comboLeerjaar);
+			panel_02.add(comboCategory);
 			
 			add(panel_01);
 			add(panel_02);
+			add(new JScrollPane(listQuiz));
+			add(new JScrollPane(listExercise));
+			
+			listQuiz.addListSelectionListener(
+					new ListSelectionListener() // anonymous inner class
+					{
+						// handle list selection events
+						public void valueChanged( ListSelectionEvent event )
+						{
+							//getContentPane().setBackground(
+							//		quizCatalog[listQuiz.getSelectedIndex()] );
+						} // end method valueChanged
+					} // end anonymous inner class
+					); // end call to addListSelectionListener
 			
 			
 			//window
@@ -93,6 +157,7 @@ public class ChangeQuizView extends JFrame implements ActionListener{
 			this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			this.setSize(750,750);
 			this.setVisible(true);
+			
 			
 		}
 
@@ -103,9 +168,17 @@ public class ChangeQuizView extends JFrame implements ActionListener{
 			JButton source = (JButton)e.getSource();
 			
 			if(source == btn_update) {
-				System.out.println("Update via cast e.getSource");
+				System.out.println("Search through e.getSource");
+				JOptionPane.showMessageDialog(ChangeQuizView.this, String.format("You pressed: %s", e.getActionCommand() ));
 			}
-			
+			else if(source == btn_edit) {
+				System.out.println("Edit through e.getSource");
+				JOptionPane.showMessageDialog(ChangeQuizView.this, String.format("You pressed: %s", e.getActionCommand() ));
+			}
+			else if(source == btn_delete) {
+				System.out.println("Delete through e.getSource");
+				JOptionPane.showMessageDialog(ChangeQuizView.this, String.format("You pressed: %s", e.getActionCommand() ));
+			}
 		}
 		
 		
