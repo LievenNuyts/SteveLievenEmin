31ca10da937e18041ebecb553baebe3334287f8a
NIME26 <nime36@hotmail.com>
2013-12-09 18:54:48 +0100

    New Class - Persistencable.java

---
+++ Opdracht_1/src/persistenty/Persistencable.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package persistenty;
+
+/**
+ * 
+ * @author Emin
+ *
+ */
+public interface Persistencable {
+	
+	public void addQuiz();
+	public void updateQuiz();
+	public void deleteQuiz();
+
+}
