6464ad328da05eaece09b4a742cc22b951e022d8
NIME26 <nime36@hotmail.com>
2013-12-15 13:42:09 +0100

    New class - ManagementInitFile.java

---
+++ Opdracht_1/src/persistenty/ManagementInitFile.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+package persistenty;
+
+import java.util.Properties;
+import javax.swing.JOptionPane;
+import java.io.FileOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/**
+ * 
+ * @author Emin
+ *
+ */
+public class ManagementInitFile {
+	
+	private Properties table;
+	
+	public ManagementInitFile(){
+		table = new Properties();
+	}
+	
+
+	public void choosePersistetyMethod(){
+		Object[] possiblePersistenties = { "Text persistenty", "MySQL persistenty" };
+		Object selection = null;
+		selection = JOptionPane.showInputDialog(null,"Select persistenty method",
+                                       "Persistenty", JOptionPane.INFORMATION_MESSAGE, null, 
+                                       possiblePersistenties, possiblePersistenties[0]);
+		if (selection !=null)
+			setPersistentyMethod((String)selection);
+	}
+	
+	public void setPersistentyMethod(String persistentyMethod){
+		try{
+			FileOutputStream out = new FileOutputStream("init.dat");
+			table.setProperty("persistentyMethod", persistentyMethod);
+			table.store(out,"init quizzes value");
+			out.close();
+		}
+		catch (IOException ex){ex.printStackTrace();}
+	}
+	
+	public String getPersistentyMethod(){
+		try{
+			FileInputStream in = new FileInputStream("init.dat");
+			table.load(in);
+			in.close();
+			Object value = table.getProperty("persistentyMethod");
+			if (value != null)
+				return (String)value;
+	
+		}
+		catch (IOException ex){ex.printStackTrace();}
+		return null;
+	}
+	
+	public static void main(String[] args) {
+		ManagementInitFile mi = new ManagementInitFile();
+		mi.choosePersistetyMethod();
+	}
+}
